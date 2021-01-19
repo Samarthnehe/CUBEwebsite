dce6ab208be75b2a8cc2c78598b2be8fa11aa9f7
Samarth Nehe <samarthnehe2000@gmail.com>
2021-01-19 23:46:28 +0530

    commit

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 
+
 app = Flask(__name__)
 fa = FontAwesome(app)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
